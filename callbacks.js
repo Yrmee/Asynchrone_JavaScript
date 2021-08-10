5bd2653425f45f28613a6773f0a3e772f2304ea8
Yrmee <80338690+Yrmee@users.noreply.github.com>
2021-08-10 15:28:37 +0200

    Opdracht: Callbacks

---
+++ callbacks.js
@@ -0,0 +1,13 @@
+// Opdracht: CallBacks
+
+function huiswerkMaken(vak, callback) {
+    console.log(`"Ok, ok, ik ga nu mijn ${vak} huiswerk maken"`);
+    setTimeout(callback, 1000);
+}
+
+function klaarMetHuiswerk() {
+    console.log("Kijk, Paps/Mams, ik ben klaar met mijn huiswerk!");
+}
+
+huiswerkMaken("Wiskunde", klaarMetHuiswerk);
+
